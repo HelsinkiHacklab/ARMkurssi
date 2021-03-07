34abfc84e0a268c6ce9ddb88bc8a46d8e0a83534
Martti Paalanen <martti.paalanen@m-tronix.fi>
2021-03-07 09:52:09 +0200

    initial commit

--- nucleo-f303_sleep/Core/Src/main.c
+++ nucleo-f303_sleep/Core/Src/main.c
@@ -1,21 +1,40 @@
 /* USER CODE BEGIN Header */
-
-/*
-Ledin vilkutus kuormittamatta pääohjelmaa
-
-Pääohjelmassa looppi joka kirjoittaa tekstiä UART2:een
-Tiedostossa stm32f3xx_it.c lisätty SysTick-keskeytyspalveluun ledin vätkytys
-*/
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  *
+  * STM32F303-prosessorin virransäästötilojen demo.
+  * F303RE osaa 3 virransäästötilaa:
+  * - Sleep Mode ( I/O-pinnien tilat säilyvät)
+  * - Stop Mode ( I/O-pinnien tilat säilyvät, HSI ja HSE-kellosignaalit pysähtyvät, muisti ja rekisterit säilyvät)
+  * - Standby Mode
+  * Tilojen virrankulutus alenee progressiivisesti samalla kuin erilaisia resursseja suljetaan
+  * Standby-tila ei enää ylläpidä muistin eikä prosessorin rekisterien tilaa. Ainoastaan
+  * reaaliaikakellon tilataltion RAM-muisti säilyy
+  *
+  */
 /* USER CODE END Header */
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdbool.h>
 #include <stdio.h>
 #include <errno.h>
-#include  <sys/unistd.h> // STDOUT_FILENO, STDERR_FILENO
 
 /* USER CODE END Includes */
 
@@ -34,6 +53,8 @@ Tiedostossa stm32f3xx_it.c lisätty SysTick-keskeytyspalveluun ledin vätkytys
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+RTC_HandleTypeDef hrtc;
+
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
@@ -44,12 +65,25 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_RTC_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+
+#include  <sys/unistd.h> // STDOUT_FILENO, STDERR_FILENO
+
+int _read(int fd, char *ptr, int len) {
+	if (fd == STDIN_FILENO ) {
+		HAL_UART_Receive(&huart2, (uint8_t *) ptr, 1, HAL_MAX_DELAY);
+	    HAL_UART_Transmit(&huart2, (uint8_t *) ptr, 1, HAL_MAX_DELAY);
+	}
+	return 1;
+}
+
 int _write(int fd, char* ptr, int len) {
   HAL_StatusTypeDef hstatus;
 
@@ -64,6 +98,19 @@ int _write(int fd, char* ptr, int len) {
   return -1;
 }
 
+uint8_t getCommandFromUSART() {
+	char cmd;
+	while ( 1 ) {
+		printf("Enter sleep mode - 1: sleep; 2: stop; 3: standby; 4: sleep+auto wakeup\r\n");
+		_read(0, &cmd, 1);
+		if ( cmd == '1' ) return 1;
+		if ( cmd == '2' ) return 2;
+		if ( cmd == '3' ) return 3;
+		if ( cmd == '4' ) return 4;
+		printf("%c did not compute\r\n", cmd);
+	}
+	return 0;	// jotta kääntyri on onnellinen, ei tähän koskaan oikeasti tulla
+}
 /* USER CODE END 0 */
 
 /**
@@ -73,7 +120,7 @@ int _write(int fd, char* ptr, int len) {
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	uint8_t command;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -95,7 +142,10 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_RTC_Init();
   /* USER CODE BEGIN 2 */
+
+  printf("\r\nARM Low Power demo\r\n\r\n");
 
   /* USER CODE END 2 */
 
@@ -103,8 +153,72 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  printf("Hello, World!\r\n");
-	  HAL_Delay( 1000);
+	  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+	  command = getCommandFromUSART();
+
+	  switch ( command ) {
+		  case 1: {
+			  printf("Entering Sleep mode now\r\n");
+			  break;
+		  }
+		  case 2: {
+			  printf("Entering Stop mode now\r\n");
+			  break;
+		  }
+		  case 3: {
+			  printf("Entering Standby mode now\r\n");
+			  break;
+		  }
+		  case 4: {
+			  printf("Entering Sleep+auto wakeup mode now\r\n");
+			  break;
+		  }
+		  default: {
+			  printf("That was not a sleep mode!\r\n");
+		  }
+	  }
+
+	  printf("Good Night!\r\n");
+	  HAL_Delay(500);
+	  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+	  // Keskeytykset herättävät prosessorin virransäästötilasta
+	  // sen takia Tick-keskeytys on pysäytettävä, muuten prosessori herää
+	  // heti millisekunnin sisällä.
+	  HAL_SuspendTick();
+
+	  // Siirrytään valittuun virransäästötilaan
+	  if ( command == 1) {
+		  // Sleep Mode: CPU core pysähtyy. Muisti ja rekisterit säilyvät
+		  // Osa oheislaitteista säilyy aktiivisina ja ne voivat herättää systeemin
+		  // Low Power regulator: oheislaitteet toimivat alennetulla kellotaajuudella
+		  HAL_PWR_EnterSLEEPMode( PWR_LOWPOWERREGULATOR_ON, PWR_SLEEPENTRY_WFI );
+	  }
+	  else if ( command == 2 ) {
+		  // Stop Mode: CPU core pysähtyy. Muisti ja rekisterit säilyvät
+		  // Useimmat oheislaitteet pysähtyvät
+		  // Reaaliaikakello on mahdollista pysäyttää virran säästämiseksi
+		  HAL_PWR_EnterSTOPMode( PWR_LOWPOWERREGULATOR_ON, PWR_SLEEPENTRY_WFI );
+		  SystemClock_Config();
+	  }
+	  else if ( command == 3 ) {
+		  HAL_PWR_EnterSTANDBYMode();
+	  }
+	  else if ( command == 4) {
+		  // Sleep Mode: CPU core pysähtyy. Muisti ja rekisterit säilyvät
+		  // Osa oheislaitteista säilyy aktiivisina ja ne voivat herättää systeemin
+		  // Low Power regulator: oheislaitteet toimivat alennetulla kellotaajuudella
+		  // Systeemi herää automaattisesti reaaliaikakellon keskeytyksestä.
+		  // Muuttuja sleepTime määrää nukkumisajan millisekunteina
+		  uint32_t sleepTime = (((uint32_t)5000) * 2314) / 1000;
+		  HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, sleepTime, RTC_WAKEUPCLOCK_RTCCLK_DIV16);
+
+		  HAL_PWR_EnterSLEEPMode( PWR_LOWPOWERREGULATOR_ON, PWR_SLEEPENTRY_WFI );
+	  }
+
+	  printf("Good Morning!\r\n");
+
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -122,11 +236,13 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the CPU, AHB and APB busses clocks 
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
@@ -135,7 +251,7 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks 
+  /** Initializes the CPU, AHB and APB buses clocks
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -148,12 +264,53 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_RTC;
   PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief RTC Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_RTC_Init(void)
+{
+
+  /* USER CODE BEGIN RTC_Init 0 */
+
+  /* USER CODE END RTC_Init 0 */
+
+  /* USER CODE BEGIN RTC_Init 1 */
+
+  /* USER CODE END RTC_Init 1 */
+  /** Initialize RTC Only
+  */
+  hrtc.Instance = RTC;
+  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+  hrtc.Init.AsynchPrediv = 127;
+  hrtc.Init.SynchPrediv = 255;
+  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
+  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
+  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
+  if (HAL_RTC_Init(&hrtc) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Enable the WakeUp
+  */
+  if (HAL_RTCEx_SetWakeUpTimer_IT(&hrtc, 0, RTC_WAKEUPCLOCK_RTCCLK_DIV16) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN RTC_Init 2 */
+
+  /* USER CODE END RTC_Init 2 */
+
 }
 
 /**
@@ -172,7 +329,7 @@ static void MX_USART2_UART_Init(void)
 
   /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
+  huart2.Init.BaudRate = 38400;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
   huart2.Init.StopBits = UART_STOPBITS_1;
   huart2.Init.Parity = UART_PARITY_NONE;
@@ -215,12 +372,22 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PA0 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
   /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 
 }
 
@@ -236,7 +403,10 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -249,10 +419,10 @@ void Error_Handler(void)
   * @retval None
   */
 void assert_failed(uint8_t *file, uint32_t line)
-{ 
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
